12379f5b488cecc2084ce897f59ebb9805814b37
zyrthofar <zyrthofar@gmail.com>
2014-08-26 21:06:49 -0400

    Rearranged the example project.

--- example/main.dart
+++ example/main.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:magnetfruit_avocadorm/avocadorm.dart';
 import 'package:magnetfruit_avocadorm/database_handler/mysql_database_handler.dart';
 import 'entities/entities.dart';
+
+var avocadorm;
 
 void main() {
   // avocadorm_guest has SELECT privileges.
@@ -10,25 +13,13 @@ void main() {
   // avocadorm_admin has SELECT, INSERT, UPDATE, and DELETE privileges.
   var databaseHandler = new MySqlDatabaseHandler('localhost', 3306, 'avocadorm_example', 'avocadorm_admin', 'password');
 
-  var avocadorm = new Avocadorm(databaseHandler)
+  avocadorm = new Avocadorm(databaseHandler)
     ..addEntitiesInLibrary('entities');
 
-  avocadorm.readById(Employee, 2, foreignKeys: ['company']).then((employee) {
-    print(JSON.encode(employee));
-
-    employee.test();
-
-//    employee.name = 'Christina Johnson';
-//
-//    avocadorm.update(employee).then((id) {
-//
-//      avocadorm.readById(Employee, 2).then((employee) {
-//        print(JSON.encode(employee));
-//      });
-//
-//    });
-
-  });
+  getEmployee(2)
+    .then(toggleName)
+    .then(updateEmployee)
+    .then(getEmployee);
 
   avocadorm.readById(Company, 3, foreignKeys: ['employees']).then((company) {
 
@@ -37,4 +28,26 @@ void main() {
   });
 
 }
-   
+
+Future getEmployee(Object id) {
+  return avocadorm.readById(Employee, id)
+    .then((e) {
+      print(JSON.encode(e));
+      return e;
+    });
+}
+
+Future toggleName(Employee employee) {
+  if (employee.name == 'Christina Johnson') {
+    employee.name = 'Rosetta Peters';
+  }
+  else {
+    employee.name = 'Christina Johnson';
+  }
+
+  return new Future.value(employee);
+}
+
+Future updateEmployee(Employee employee) {
+  return avocadorm.update(employee);
+}
